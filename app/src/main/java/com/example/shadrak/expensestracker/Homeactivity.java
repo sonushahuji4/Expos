251b48ee818b6a73b2b10700c54fff64a69c1d7f
shadrak98 <gurupnor.shadrak@gmail.com>
2019-08-19 16:07:14 +0530

    Bottom nav

--- app/src/main/java/com/example/shadrak/expensestracker/Homeactivity.java
+++ app/src/main/java/com/example/shadrak/expensestracker/Homeactivity.java
@@ -8,19 +8,26 @@ import android.widget.Button;
 import com.google.firebase.auth.FirebaseAuth;
 
 import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.view.MenuItem;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.Toast;
 
 // imports from line 10-18 are for Navigation Drawer, dlt it if doesn't work
 
 public class Homeactivity extends AppCompatActivity {
     Button btnlogout;
+    FrameLayout frameLayout;
+    BottomNavigationView bottomNavigationView;
+    TextView textView;
     FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
 
@@ -30,61 +37,79 @@ public class Homeactivity extends AppCompatActivity {
         setContentView(R.layout.activity_homeactivity);
 
         btnlogout = findViewById(R.id.button5);
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        frameLayout = findViewById(R.id.framelayout);
+        textView = findViewById(R.id.textview);
 
-        btnlogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Intent inToMain = new Intent(Homeactivity.this, MainActivity.class);
-                startActivity(inToMain);
+        //btnlogout.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View view) {
+        //        FirebaseAuth.getInstance().signOut();
+        //        Intent inToMain = new Intent(Homeactivity.this, MainActivity.class);
+        //        startActivity(inToMain);
+        //    }
+        //});
+
+        textView.setText("Receipts");
+        frameLayout.setBackgroundResource(R.color.colorPrimaryDark);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(naviCustomListView);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener naviCustomListView = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.bill:
+                    textView.setText("Bills");
+                    frameLayout.setBackgroundResource(R.color.colorAccent);
+                    break;
+                case R.id.reports:
+                    textView.setText("Reports");
+                    frameLayout.setBackgroundResource(R.color.colorPrimaryDark);
+                    break;
+                case R.id.summary:
+                    textView.setText("Summary");
+                    frameLayout.setBackgroundResource(R.color.colorAccent);
+                    break;
+                case R.id.events:
+                    textView.setText("Events");
+                    frameLayout.setBackgroundResource(R.color.colorPrimaryDark);
+                    break;
             }
-        });
-    }
-}
-
-// Code for Navigation Drawer
-// Delete this if not working
-
-public class NavigationDrawer extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
-    Toolbar toolbar;
-    DrawerLayout drawerLayout;
-    NavigationView navigationView;
+            return true;
+        }
+    };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.example_navigation_drawer); // example_navigation_drawer
-
-        toolbar = findViewById(R.id.tool_bar); //tool_bar
-        setSupportActionBar(toolbar);
-
-        drawerLayout = findViewById(R.id.drawerlayout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-
-        navigationView = findViewById(R.id.navigation_view);
-        navigationView.setNavigationItemSelectedListener(this);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu,menu);
+        return true;
     }
 
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        switch (menuItem.getGroupId()) {
-            case R.id.navi_bills;
-                Toast.makeText(this, "Bills Selected", Toast.LENGTH_SHORT).show();
-                break;
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.logout:
+                Button option;
+                option = findViewById(R.id.logout);
 
-            case R.id.navi_bills;
-                Toast.makeText(this, "Bills Selected", Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.navi_bills;
-                Toast.makeText(this, "Bills Selected", Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.navi_bills;
-                Toast.makeText(this, "Bills Selected", Toast.LENGTH_SHORT).show();
-                break;
+                option.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        FirebaseAuth.getInstance().signOut();
+                        Intent inToMain = new Intent(Homeactivity.this, MainActivity.class);
+                        startActivity(inToMain);
+                    }
+                });
+                return true;
+            case R.id.help:
+                Toast.makeText(this, "Help option selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
+
